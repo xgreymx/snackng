2bc2cf7b767acbc9c6dcddccec87c07940d12460
Emilio <emilioalejandrocs6@gmail.com>
2026-09-06 22:44:22 -0600

    close btn integrated

--- projects/snackng/src/lib/icons.ts
+++ projects/snackng/src/lib/icons.ts
@@ -11,3 +11,7 @@ export const SNACKNG_ICONS: Record<SnackngBuiltInType, string> = {
     'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m1 15h-2v-2h2zm0-4h-2V7h2z',
   info: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m1 15h-2v-6h2zm0-8h-2V7h2z',
 };
+
+export const SNACKNG_CLOSE_ICON =
+  'M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z';
+
